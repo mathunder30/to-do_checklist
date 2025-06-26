ca2698bdb03dcb62fe53f6637964246f693c6d44
mathunder30 <mateustlopes11@gmail.com>
2025-06-26 10:33:46 -0300

    criando os arquivos para banco de dados

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,7 @@
-import express, {Request, Response} from 'express'
+import express, {Request, Response} from 'express';
+import { PrismaClient } from '@prisma/client';
+import cors from 'cors';
+/*import {dotenv} from 'dotenv'*/
+
+const app = express();
+app.use(cors());
